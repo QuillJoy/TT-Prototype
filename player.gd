05f996cdd848155139dd59f36f8116bb901aef47
John Quilantang <quill@Johns-Air.lan>
2024-05-13 21:24:19 -0700

    Move rotation to player

--- player.gd
+++ player.gd
@@ -1,6 +1,12 @@
 extends CharacterBody3D
 @onready var joystick = $"../CanvasLayer/Joystick"
+@export var rotate_speed: float = 1.0
 
+@export var can_rotate: bool
+
+var joystick_pressed: bool
+
+var touch_points: Dictionary = {}
 var speed = 5
 
 func _physics_process(delta):
@@ -15,6 +21,34 @@ func _physics_process(delta):
 		
 	move_and_slide()
 
-func _unhandle_input(event):
-	if event is InputEventScreenDrag:
-		print("screen drag")
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		handle_touch(event)
+	elif event is InputEventScreenDrag:
+		handle_drag(event)
+
+		
+func handle_touch(event: InputEventScreenTouch):
+	if event.pressed:
+		touch_points[event.index] = event.position
+	else:
+		touch_points.erase(event.index)
+		
+func handle_drag(event: InputEventScreenDrag):
+	touch_points[event.index] = event.position
+	if !joystick_pressed:
+		if touch_points.size() == 1:
+			if can_rotate:
+				rotate_object_local(Vector3.UP, event.relative.x * 0.001)
+				print(self.rotation_degrees)
+	
+
+func _on_joystick_joystick_pressed():
+	joystick_pressed = true
+
+
+func _on_joystick_joystick_released():
+	joystick_pressed = false
+
+
