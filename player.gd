2a7b8b2073b5eaa7cef6a864709c3da2d671cc7b
John-Quilantang <johnalf_quilantang@csu.fullerton.edu>
2024-05-14 12:33:53 -0700

    Add first person view

--- player.gd
+++ player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody3D
 @onready var joystick = $"../CanvasLayer/Joystick"
 @export var rotate_speed: float = 1.0
+@onready var first_person_camera_pivot = $FirstPersonCameraPivot
+
+@onready var first_person_camera = $FirstPersonCameraPivot/FirstPersonCamera
 
 @export var can_rotate: bool
 
@@ -15,13 +18,23 @@ func _physics_process(delta):
 	var direction = joystick.posVector
 	direction.z = direction.y
 	direction.y = 0
-	var camera_rotation_y = camera_3d.global_transform.basis.get_euler().y
+	
+	if(first_person_camera.current):
+		var camera_rotation_y = first_person_camera.global_transform.basis.get_euler().y
 
-	direction = direction.rotated(Vector3.UP,camera_rotation_y).normalized()
+		direction = direction.rotated(Vector3.UP,camera_rotation_y).normalized()	
+	else:
+	
+		var camera_rotation_y = camera_3d.global_transform.basis.get_euler().y
+
+		direction = direction.rotated(Vector3.UP,camera_rotation_y).normalized()
 
 	if direction:
 		velocity = direction * speed
-		$Pivot.look_at($Pivot.global_transform.origin + direction, Vector3.UP)
+		if(first_person_camera.current):
+			pass
+		else:
+			$Pivot.look_at($Pivot.global_transform.origin + direction, Vector3.UP)
 	else:
 		velocity = Vector3(0,0,0)
 		
@@ -47,8 +60,15 @@ func handle_drag(event: InputEventScreenDrag):
 	if !joystick_pressed:
 		if touch_points.size() == 1:
 			if can_rotate:
-				camera_pivot.rotate_object_local(Vector3.UP, event.relative.x * -0.001) 
-				camera_3d.rotate_object_local(Vector3.RIGHT, event.relative.y * -0.001)
+				if(first_person_camera.current):
+					first_person_camera_pivot.rotate_object_local(Vector3.UP, event.relative.x * -0.001) 
+					first_person_camera.rotate_object_local(Vector3.RIGHT, event.relative.y * -0.001)
+					$Pivot.look_at(first_person_camera.global_transform.origin + -first_person_camera.global_transform.basis.z, Vector3.UP)
+					#$Pivot.look_at(first_person_camera.position, Vector3.RIGHT)
+				else:
+					
+					camera_pivot.rotate_object_local(Vector3.UP, event.relative.x * -0.001) 
+					camera_3d.rotate_object_local(Vector3.RIGHT, event.relative.y * -0.001)
 				#var camera_rotation = camera_pivot.global_transform.basis.get_euler()
 				#print(camera_rotation)
 				#var local_right = camera_pivot.global_transform.basis.x
@@ -71,6 +91,9 @@ func _on_button_pressed():
 	var new_size = camera_3d.size * zoom_factor
 
 	camera_3d.size = new_size
+	
+	if(camera_3d.size < 10.0):
+		first_person_camera.current = true
 
 
 func _on_zoom_out_pressed():
@@ -79,4 +102,8 @@ func _on_zoom_out_pressed():
 	var new_size = camera_3d.size * zoom_factor
 
 	camera_3d.size = new_size
+	
+	if(camera_3d.size > 10.0):
+		camera_3d.current = true
+	
 
